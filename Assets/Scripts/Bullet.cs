6eea25e9fb238360cf15ee82caceda1bd218ca1c
Aliaksei-Tryfanau <altrishka@gmail.com>
2025-02-07 08:42:29 -0500

    Melee enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,7 +1,10 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float impactForce;
+
     private BoxCollider cd;
     private Rigidbody rb;
     private MeshRenderer meshRenderer;
@@ -23,8 +26,10 @@ public class Bullet : MonoBehaviour
         trailRenderer = GetComponent<TrailRenderer>();
     }
 
-    public void BulletSetup(float flyDistance)
+    public void BulletSetup(float flyDistance, float impactForce)
     {
+        this.impactForce = impactForce;
+
         bulletDisabled = false;
         cd.enabled = true;
         meshRenderer.enabled = true;
@@ -65,7 +70,26 @@ public class Bullet : MonoBehaviour
     {
         CreateImpactFx(collision);
         ReturnBulletToPool();
+
+        Enemy enemy = collision.gameObject.GetComponentInParent<Enemy>();
+        Enemy_Shield shield = collision.gameObject.GetComponent<Enemy_Shield>();
+
+        if (shield != null)
+        {
+            shield.ReduceDurability();
+            return;
+        }
+
+        if (enemy != null)
+        {
+            Vector3 force = rb.velocity.normalized * impactForce;
+            Rigidbody hitRigidbody = collision.collider.attachedRigidbody;
+
+            enemy.GetHit();
+            enemy.DeathImpact(force, collision.contacts[0].point, hitRigidbody);
+        }
     }
+
 
     private void ReturnBulletToPool() => ObjectPool.instance.ReturnObject(gameObject);
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Top-down third person shooter with following features:
 - multiple weapons (pistol, revolver, shotgun, assault rifle) with unique shooting mechanics;
 - character animation rigging logic with custom animation layers for different weapons;
-- world interactable objects (pickups, doors, terminals, etc.);
+- world interactable objects (pickups, doors, terminals, weapons, etc.);
